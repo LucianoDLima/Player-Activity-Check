01ad729f77278ace66ce50e133906780888c6b81
Luciano Lima <ljr.luciano.junior@outlook.com>
2025-06-19 21:45:41 -0300

    chore: comment out code
    - code will still be used later on

--- src/bot/features/checkActivity.ts
+++ src/bot/features/checkActivity.ts
@@ -1,54 +1,61 @@
-import { Message } from "discord.js";
-import { formatName } from "../../util/formatNames";
-import { checkPlayerActivity } from "../../scraper/checkPlayerActivity";
-import { calculateDaysSinceLastActivity } from "../../util/formatDate";
-import { findAllPlayers, updatePlayerLastOnline } from "../../db/players";
+// import { Message } from "discord.js";
+// import { formatName } from "../../util/formatNames";
+// import { checkPlayerActivity } from "../../scraper/checkPlayerActivity";
+// import { calculateDaysSinceLastActivity } from "../../util/formatDate";
+// import { findAllPlayers } from "../../db/players";
+// import { fetchPlayerSummary } from "./onApi";
 
-export async function checkAllPlayersActivity(message: Message) {
-  try {
-    const allPlayers = await findAllPlayers();
+/**
+ * Moved the logic to scanClanActivity, however this might still be needed.
+ * My idea is to first try scanClanActivity, and if it fails, scrape the runemetrics page directly with this function.
+ * The scanClanActivity uses their api, which is more reliable and faster, but I hear can be rate limited tho it is very inconsistent.
+ */
 
-    const totalMembers = allPlayers.length;
-    const progressMessage = await message.reply(
-      `Checking activity: 0/${totalMembers}`,
-    );
-    message.reply(
-      `Checking all players' activity (${totalMembers} members)... Please wait a moment.`,
-    );
+// export async function checkAllPlayersActivity(message: Message) {
+//   try {
+//     const allPlayers = await findAllPlayers();
 
-    let current = 1;
+//     const totalMembers = allPlayers.length;
+//     const progressMessage = await message.reply(
+//       `Checking activity: 0/${totalMembers}`,
+//     );
+//     message.reply(
+//       `Checking all players' activity (${totalMembers} members)... Please wait a moment.`,
+//     );
 
-    for (const member of allPlayers) {
-      try {
-        if (member.isException) {
-          console.log(`Skipping ${member.name} due to rank: ${member.rank}`);
-          current++;
+//     let current = 1;
 
-          continue;
-        }
+//     for (const member of allPlayers) {
+//       try {
+//         if (member.isException) {
+//           console.log(`Skipping ${member.name} due to rank: ${member.rank}`);
+//           current++;
 
-        const progressText = `Checking activity: ${current}/${totalMembers} (${member.name})`;
-        await progressMessage.edit(progressText);
+//           continue;
+//         }
 
-        const formattedName = formatName(member.name);
-        const lastActiveDate = await checkPlayerActivity(formattedName);
-        const daysSinceLastActivity = calculateDaysSinceLastActivity(lastActiveDate);
+//         const progressText = `Checking activity: ${current}/${totalMembers} (${member.name})`;
+//         await progressMessage.edit(progressText);
 
-        console.log(
-          `${member.name} last active on DB ${daysSinceLastActivity ?? "Never"} days ago`,
-        );
+//         const formattedName = formatName(member.name);
+//         const lastActiveDate = await checkPlayerActivity(formattedName);
+//         const daysSinceLastActivity = calculateDaysSinceLastActivity(lastActiveDate);
 
-        if (lastActiveDate) {
-          await updatePlayerLastOnline(member.name, lastActiveDate);
-          console.log(`Updated ${formattedName} with lastOnline: ${lastActiveDate}`);
-        }
-      } catch (memberError) {
-        console.error(`Error processing ${member.name}:`, memberError);
-      }
+//         console.log(
+//           `${member.name} last active on DB ${daysSinceLastActivity ?? "Never"} days ago`,
+//         );
 
-      current++;
-    }
-  } catch (error) {
-    console.error("Error checking all players' activity:", error);
-  }
-}
+//         if (lastActiveDate) {
+//           // await updatePlayerOnlineStatus(member.name, undefined, lastActiveDate);
+//           console.log(`Updated ${formattedName} with lastOnline: ${lastActiveDate}`);
+//         }
+//       } catch (memberError) {
+//         console.error(`Error processing ${member.name}:`, memberError);
+//       }
+
+//       current++;
+//     }
+//   } catch (error) {
+//     console.error("Error checking all players' activity:", error);
+//   }
+// }
